a08d86cd8368181ae99be24f152fab696a48010f
Liel Sne <liel.sne@ryzebeyond.com>
2026-06-26 17:45:18 +0300

    Configure sources and refactor staging models to use source()

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,14 @@
-    select
-        id as customer_id,
-        first_name,
-        last_name
+    with source as (
+        select * from {{ source('jaffle_shop', 'customers') }}
+    ),
 
-    from {{ source('jaffle_shop', 'customers') }}
+    renamed as (    
+        select
+            id as customer_id,
+            first_name,
+            last_name
+
+        from source
+    )
+
+    select * from renamed
